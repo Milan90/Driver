d531a90a76a718e10b11e6045ce4a00051ca13ea
Milan Nowak <milan.nowak90@gmail.com>
2019-02-21 17:40:29 +0100

    Advice details serializer corrects

--- Driver/DriverAdvice/serializers.py
+++ Driver/DriverAdvice/serializers.py
@@ -15,22 +15,31 @@ class VideoSerializer(serializers.ModelSerializer):
 
 
 class AdviceSerializer(serializers.ModelSerializer):
-    images = ImageSerializer(many=True)
-    video = VideoSerializer(many=True)
+    images = ImageSerializer(many=True, required=False)
+    video = VideoSerializer(many=True, required=False)
 
     class Meta:
         model = Advice
         fields = '__all__'
 
     def create(self, validated_data):
-        images_data = validated_data.pop('images')
-        videos_data = validated_data.pop('video')
+        try:
+            images_data = validated_data.pop('images')
+        except KeyError:
+            images_data = None
+        try:
+            videos_data = validated_data.pop('video')
+        except KeyError:
+            videos_data = None
+
         advice = Advice.objects.create(**validated_data)
 
-        for image_data in images_data:
-            Images.objects.create(advice=advice, **image_data)
+        if images_data:
+            for image_data in images_data:
+                Images.objects.create(advice=advice, **image_data)
 
-        for video_data in videos_data:
-            Video.objects.create(advice=advice, **video_data)
+        if videos_data:
+            for video_data in videos_data:
+                Video.objects.create(advice=advice, **video_data)
 
         return advice
