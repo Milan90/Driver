e2ac88b1a4d630fe39dcd027122c763564af76e1
Milan Nowak <milan.nowak90@gmail.com>
2019-02-21 15:13:11 +0100

    models and Advice model list API view

--- Driver/DriverAdvice/views.py
+++ Driver/DriverAdvice/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class AdviceList(APIView):
+    def get(self, request, format=None):
+        advices = Advice.objects.all()
+        serializer = AdviceSerializer(advices, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = AdviceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
