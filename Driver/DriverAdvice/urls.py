c50e1d75bb5e1f3cf18744ef0322e73edf7ed9c9
Milan Nowak <milan.nowak90@gmail.com>
2019-02-21 17:20:30 +0100

    Advice details url

--- Driver/DriverAdvice/urls.py
+++ Driver/DriverAdvice/urls.py
@@ -4,6 +4,7 @@ from Driver.DriverAdvice import views
 
 urlpatterns = [
     url(r'^advices/$', views.AdviceList.as_view()),
+    url(r'^advices/(?P<pk>\d+)$', views.AdviceDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
