e4e52b0a1c73da6e5639cf87cafee2f25f36f181
Milan Nowak <milan.nowak90@gmail.com>
2019-02-22 17:50:43 +0100

    model and serializer changes

--- Driver/DriverAdvice/models.py
+++ Driver/DriverAdvice/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Advice(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    image = models.ImageField(upload_to="images/", blank=True)
+    video = models.FileField(upload_to="videos/", blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -11,24 +13,6 @@ class Advice(models.Model):
 
     class Meta:
         ordering = ('created',)
-
-
-class Images(models.Model):
-    image = models.ImageField(upload_to="images/", blank=True)
-    advice = models.ForeignKey(Advice, related_name='images', on_delete=models.CASCADE)
-    uploaded = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.image.name
-
-
-class Video(models.Model):
-    video_file = models.FileField(upload_to="videos/", blank=True)
-    advice = models.ForeignKey(Advice, related_name='videos', on_delete=models.CASCADE)
-    uploaded = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.video_file.name
 
 
 class Quiz(models.Model):
@@ -51,7 +35,7 @@ class Answers(models.Model):
     answer_text = models.CharField(max_length=150)
     question = models.ForeignKey(QuizQuestion, on_delete=models.CASCADE)
     is_correct = models.BooleanField(default=False)
-    image = models.ImageField(upload_to="images/", blank=True)
+    image = models.ImageField(upload_to="images/answers/", blank=True)
 
     def __str__(self):
         return self.answer_text
--- Driver/DriverAdvice/serializers.py
+++ Driver/DriverAdvice/serializers.py
@@ -2,44 +2,35 @@ from rest_framework import serializers
 from .models import *
 
 
-class ImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Images
-        fields = '__all__'
-
-
-class VideoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Video
-        fields = '__all__'
-
-
 class AdviceSerializer(serializers.ModelSerializer):
-    images = ImageSerializer(many=True, required=False)
-    video = VideoSerializer(many=True, required=False)
+    image = serializers.ImageField(allow_empty_file=True, use_url=True, default='')
+    video = serializers.FileField(allow_empty_file=True, use_url=True, default='')
 
     class Meta:
         model = Advice
         fields = '__all__'
 
+
+"""
     def create(self, validated_data):
+        print("validated data:", validated_data)
         try:
-            images_data = validated_data.pop('images')
+            image_data = validated_data.pop('image')
         except KeyError:
-            images_data = None
+            image_data = None
         try:
-            videos_data = validated_data.pop('video')
+            video_data = validated_data.pop('video')
         except KeyError:
-            videos_data = None
+            video_data = None
 
         advice = Advice.objects.create(**validated_data)
 
-        if images_data:
-            for image_data in images_data:
-                Images.objects.create(advice=advice, **image_data)
-
-        if videos_data:
-            for video_data in videos_data:
-                Video.objects.create(advice=advice, **video_data)
+        print("image:", image_data)
+        if image_data:
+            Images.objects.create(advice=advice, image=image_data)
+        print("video:", video_data)
+        if video_data:
+            Video.objects.create(advice=advice, video_file=video_data)
 
         return advice
+"""
